f640e50df944edfc6bf650aea01cda3285c4b925
khazarmammadov071 <khazarmammadov5@gmail.com>
2023-05-07 03:14:50 +0400

    convertor 10 t0 3 with StringBuilder

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int b, sum = 0, cnt = 1;
-        int a = sc.nextInt();
+        StringBuilder ternary = new StringBuilder();
 
+        int decimal = sc.nextInt();
 
-        while (a != 0) {
-            b = a % 3;
-            sum = sum * 10 + b;
-            if (sum == 0 && b == 0) {
-                cnt = cnt * 10;
-            }
-            a = a / 3;
-
+        while (decimal != 0) {
+            ternary.append(decimal % 3);
+            decimal /= 3;
         }
-
-        int temp = sum * cnt;
-
-        while (temp != 0) {
-            System.out.print(temp % 10);
-            temp /= 10;
-        }
-
+        System.out.println(ternary.reverse());
 
     }
 
