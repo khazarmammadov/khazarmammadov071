78c9644b5c387bac0f69e1f482b568c7db2533f1
khazarmammadov071 <khazarmammadov5@gmail.com>
2023-05-07 03:26:54 +0400

    convertor 10 t0 3 with int

---
+++ src/DecimalToTernary.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class DecimalToTernary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int b, sum = 0, cnt = 1;
+        int a = sc.nextInt();
+
+
+        while (a != 0) {
+            b = a % 3;
+            sum = sum * 10 + b;
+            if (sum == 0 && b == 0) {
+                cnt = cnt * 10;
+            }
+            a = a / 3;
+
+        }
+
+        int temp = sum * cnt;
+
+        while (temp != 0) {
+            System.out.print(temp % 10);
+            temp /= 10;
+        }
+
+
+    }
+
+}
+
