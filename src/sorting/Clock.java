df0dfb960feb66ab8dd45987b62c0957ec7e60e3
khazarmammadov071 <khazarmammadov5@gmail.com>
2023-05-07 14:56:49 +0400

    tasks clocl

---
+++ src/sorting/Clock.java
@@ -0,0 +1,18 @@
+package sorting;
+
+public class Clock {
+    public static void main(String[] args) {
+        int hour = 13;
+        int min = 25;
+
+        double DegreeMin = min * 6;
+        double HourWay = (double) min / 12;
+        double HourDegree = (HourWay+(double)((hour-12)*5)) * 6 ;
+        double Degree = DegreeMin - HourDegree;
+
+        System.out.println(HourWay);
+
+        System.out.println(Degree);
+
+    }
+}
