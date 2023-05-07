6da5d007272ee9d9f35d53b904144f637c445377
khazarmammadov071 <khazarmammadov5@gmail.com>
2023-05-07 15:40:47 +0400

    tasks search

---
+++ src/sorting/Search.java
@@ -0,0 +1,34 @@
+package sorting;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Search {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = {4, 5, 6, 7,  9, 10};
+        int[] newArr = new int[arr.length + 1];
+
+        int temp = 0;
+
+        int num = sc.nextInt();
+
+        for (int i = 0; i < arr.length-1; i++) {
+            if (num > arr[i] && num < arr[i + 1]) {
+                temp = i + 1;
+                newArr[i] = arr[i];
+                newArr[temp] = num;
+                break;
+            }
+            newArr[i] = arr[i];
+        }
+        if (temp == 0) {
+            return;
+        }
+        for (int i = temp; i < arr.length; i++) {
+            newArr[i+1] = arr[i];
+
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(newArr));
+
+    }
+}
