e2da7dcc836f177db28f595233984f931e037691
khazarmammadov071 <khazarmammadov5@gmail.com>
2023-05-15 02:09:42 +0400

    tasks Lesson7 EBOB

---
+++ src/lesson7/Hcf.java
@@ -0,0 +1,19 @@
+package lesson7;
+
+import java.util.Scanner;
+public class Hcf {
+    public static int hcf(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(hcf(a, b));
+    }
+}
