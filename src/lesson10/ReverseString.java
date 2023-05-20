a076dbe44af31c4feb4a4fb7c63a2ca5168ca831
khazarmammadov071 <khazarmammadov5@gmail.com>
2023-05-20 20:36:03 +0400

    tasks Lesson10 ReverseString

---
+++ src/lesson10/ReverseString.java
@@ -0,0 +1,17 @@
+package lesson10;
+
+public class ReverseString {
+
+    public static void reverse(String str,int index) {
+        if (index < 0) {
+            return;
+        }
+        System.out.print(str.charAt(index));
+        reverse(str,index-1);
+    }
+
+    public static void main(String[] args) {
+        String str = "Hello World!!!";
+        reverse(str,str.length()-1);
+    }
+}
