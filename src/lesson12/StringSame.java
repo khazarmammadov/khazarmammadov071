338921e74461453084f196369e33ce5919cae688
khazarmammadov071 <khazarmammadov5@gmail.com>
2023-05-25 21:09:54 +0400

    tasks Lesson12 verilmis String deki en cox istifade olunan herfi tapmaq(map ile)

---
+++ src/lesson12/StringSame.java
@@ -0,0 +1,33 @@
+package lesson12;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StringSame {
+    public static void main(String[] args) {
+        String str = "Now traverse frequencies.";
+        HashMap<Character, Integer> map = new HashMap<>();
+        char[] arr = str.toCharArray();
+
+        for (char c : arr) {
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry :
+                map.entrySet()) {
+
+            if (entry.getValue() > 1) {
+                System.out.println(entry.getKey()
+                        + " : "
+                        + entry.getValue());
+            }
+        }
+    }
+
+
+    }
+
